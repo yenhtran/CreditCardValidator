0cf5dc499362d41ca3550467d7794856cf3b8692
Yen Tran <yen.tran@autodesk.com>
2017-04-11 08:42:20 -0700

    adds and sets up mocha testing framework

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect,
+		creditCards = require('../index.js');
+
+describe('brainTree program', function(){
+	it ('should work!', function() {
+		expect(true).to.be.true;
+	});
+});

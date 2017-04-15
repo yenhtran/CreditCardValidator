1be5e81b3ac7ba697172b624d9fe446a56dd6c4c
Yen Tran <yen.tran@autodesk.com>
2017-04-15 00:15:05 -0700

    moves processing and summary logic to processor.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var chalk = require('chalk'),
+const chalk = require('chalk'),
     clear = require('clear'),
     figlet = require('figlet'),
     fs = require('fs'),
     readline = require('readline'),
     CreditCard = require('./lib/creditCard.js'),
     CardValidator = require('./lib/cardValidator.js'),
-    accounts = [],
-    transactionRequests = [];
+    processor = require('./lib/processor.js');
+
+let transactionRequests = [],
+		accounts = [];
 
 clear();
 console.log(
@@ -21,73 +23,6 @@ function readContent(fileName, callback) {
     fs.readFile('./data/' + fileName, 'utf8', function(err, content) {
         if (err) return console.log(err);
         callback(null, content);
-    });
-}
-
-function findAccount(name) {
-    return accounts.find(function(account) {
-        return (account.name === name) ? true : false
-    });
-}
-
-function compare(card1, card2) {
-    if (card1.name < card2.name)
-        return -1;
-    if (card1.name > card2.name)
-        return 1;
-    return 0;
-}
-
-function processTransactions(transactions) {
-		const reName = /(?:\S+\s+){1}(\S+)/,
-					reAction = /(^|\W)Add|Charge|Credit($|\W)/g,
-					reCardNumber = /\d{1,16}/g,
-					reCardLimit = /\$\d+/g,
-					reAmount = /\$\d+/g;
-
-    transactions.forEach(function(transaction) {
-        let name = transaction.match(reName)[1],
-            action = transaction.match(reAction)[0].trim();
-
-        if (action === 'Add') {
-            let cardNumber = transaction.match(reCardNumber)[0],
-                cardLimit = parseInt(transaction.match(reCardLimit)[0].slice(1)),
-                cardValidator = new CardValidator(cardNumber);
-
-            cardValidator.lunh10Validate() ? accounts.push(new CreditCard(name, cardNumber, cardLimit)) : accounts.push(new CreditCard(name, 'error', ''))
-
-        } else {
-
-            if (findAccount(name)) {
-                let currentCreditCard = findAccount(name),
-                    amount = parseInt(transaction.match(reAmount)[0].slice(1)),
-                    cardValidator = new CardValidator(currentCreditCard.cardNumber);
-
-                if (action === 'Charge' && cardValidator.lunh10Validate()) {
-                    currentCreditCard.charge(amount)
-                } else if (action === 'Credit' && cardValidator.lunh10Validate()) {
-                    currentCreditCard.credit(amount)
-                }
-            } else {
-                return false;
-            }
-        };
-    });
-}
-
-function generateSummary(accounts) {
-    let userSummaries = [];
-
-    accounts.sort(compare).forEach(function(account) {
-        if (account.cardNumber !== 'error') {
-            userSummaries.push('' + account.name + ': $' + account.balance + '');
-        } else {
-            userSummaries.push('' + account.name + ': ' + account.cardNumber + '');
-        }
-    });
-
-    userSummaries.forEach(function(summary) {
-        process.stdout.write(`${summary}` + '\n');
     });
 }
 
@@ -104,9 +39,9 @@ process.stdin.on('data', function(data) {
 
 			readContent(filePath, function(err, content) {
 				let transactions = content.split('\n');
-			  processTransactions(transactions);
+			  accounts = processor.processTransactions(transactions);
 			  process.stdout.write(`==== SUMMARY ====\n`);
-			  generateSummary(accounts);
+			  processor.generateSummary(accounts);
 			  process.exit();
 			});
     }
@@ -116,9 +51,9 @@ process.stdin.on('data', function(data) {
     		process.exit();
     		break;
     	case 'summary':
-    		processTransactions(transactionRequests);
+    		accounts = processor.processTransactions(transactionRequests);
     		process.stdout.write(`==== SUMMARY ====\n`);
-    		generateSummary(accounts);
+    		processor.generateSummary(accounts);
     		process.exit();
     		break;
     	default:
